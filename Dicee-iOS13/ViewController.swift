d8f8e55a594268a3ad25f1302a2fdc0ad6a2a9a4
Sunbekova <suanbekova05@gmail.com>
2024-10-08 21:46:16 +0500

    dice app finished

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -4,17 +4,40 @@
 //
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
+//  Finish by Aisha Suanbekova
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceLeft: UIImageView!
+    @IBOutlet weak var diceRight: UIImageView!
+    
+    let dices: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonToRoll(_ sender: Any) {
+        diceImageChanger()
+    }
+    
+    func diceImageChanger(){
+           if let leftDice = dices.randomElement(), let rightDice = dices.randomElement() {
+                       diceLeft.image = UIImage(named: leftDice)
+                       diceRight.image = UIImage(named: rightDice)
+                   } else {
+                       print("No dice images found.")
+                   }
+       }
+    
+    //bonus.
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        diceImageChanger()
+    }
 
 }
 
